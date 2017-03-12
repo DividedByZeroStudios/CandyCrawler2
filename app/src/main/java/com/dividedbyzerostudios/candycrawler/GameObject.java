354a52594371afcf6f5a78ca65b3b21ea7760ab5
DividedByZeroStudios <dividedbyzerostudios@gmail.com>
2017-03-12 17:21:26 +0000

    Episode 2
    Collisions made, maybe want to change tap moving player?

---
+++ app/src/main/java/com/dividedbyzerostudios/candycrawler/GameObject.java
@@ -0,0 +1,12 @@
+package com.dividedbyzerostudios.candycrawler;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by Georg on 12/03/2017.
+ */
+
+public interface GameObject {
+    public void draw(Canvas canvas);
+    public void update();
+}
---
+++ app/src/main/java/com/dividedbyzerostudios/candycrawler/RectPlayer.java
@@ -0,0 +1,41 @@
+package com.dividedbyzerostudios.candycrawler;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+
+/**
+ * Created by Georg on 12/03/2017.
+ */
+
+public class RectPlayer implements GameObject {
+
+    private Rect rectangle;
+    private int color;
+
+    public Rect getRectangle() {
+        return rectangle;
+    }
+
+    public RectPlayer (Rect rectangle, int color) {
+        this.rectangle = rectangle;
+        this.color = color;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(color);
+        canvas.drawRect(rectangle, paint);
+}
+
+@Override
+public void update() {
+
+    }
+
+    public void update(Point point) {
+        rectangle.set(point.x - rectangle.width()/2, point.y - rectangle.height()/2, point.x + rectangle.width()/2, point.y + rectangle.height()/2); //set with left,right,top,bottom
+    }
+}
