30e2e1a660d3551b32562fb5ee34afdcb640a477
DividedByZeroStudios <dividedbyzerostudios@gmail.com>
2017-03-13 22:17:22 +0000

    Episode 3
    Maybe add doors that you can shoot at? higher playuer damage means more
    rooms

---
+++ app/src/main/java/com/dividedbyzerostudios/candycrawler/Constants.java
@@ -0,0 +1,10 @@
+package com.dividedbyzerostudios.candycrawler;
+
+/**
+ * Created by Georg on 13/03/2017.
+ */
+
+public class Constants {
+    public static int SCREEN_WIDTH;
+    public  static  int SCREEN_HEIGHT;
+}
--- app/src/main/java/com/dividedbyzerostudios/candycrawler/GamePanel.java
+++ app/src/main/java/com/dividedbyzerostudios/candycrawler/GamePanel.java
@@ -18,6 +18,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
     private RectPlayer player;
     private Point playerPoint;
+    private ObstacleManager obstacleManager;
 
     public GamePanel(Context context) {
         super(context);
@@ -28,6 +29,8 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
         player = new RectPlayer(new Rect(100, 100, 200, 200), Color.rgb(255, 0, 0));
         playerPoint = new Point(150, 150);
+
+        obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
 
         setFocusable(true);
     }
@@ -71,7 +74,9 @@ public boolean onTouchEvent (MotionEvent event) {
 }
 
     public void update() {
+
         player.update(playerPoint);
+        obstacleManager.update();
     }
     @Override
     public void draw(Canvas canvas) {
@@ -79,5 +84,6 @@ public boolean onTouchEvent (MotionEvent event) {
         canvas.drawColor(Color.WHITE);
 
         player.draw(canvas);
+        obstacleManager.draw(canvas);
     }
 }
