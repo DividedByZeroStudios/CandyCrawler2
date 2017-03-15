5167fe20ac4aee3cc3b3d2e31b4baeba247d1648
DividedByZeroStudios <dividedbyzerostudios@gmail.com>
2017-03-15 13:09:37 +0000

    Episode 4

--- app/src/main/java/com/dividedbyzerostudios/candycrawler/GamePanel.java
+++ app/src/main/java/com/dividedbyzerostudios/candycrawler/GamePanel.java
@@ -3,11 +3,14 @@ package com.dividedbyzerostudios.candycrawler;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import static com.dividedbyzerostudios.candycrawler.Constants.SCREEN_HEIGHT;
 
 /**
  * Created by Georg on 12/03/2017.
@@ -15,10 +18,18 @@ import android.view.SurfaceView;
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
+    private Rect r = new Rect();
 
     private RectPlayer player;
     private Point playerPoint;
     private ObstacleManager obstacleManager;
+
+    private boolean movingPlayer = false;
+
+    private boolean gameOver = false;
+    private long gameOverTime;
+
+
 
     public GamePanel(Context context) {
         super(context);
@@ -28,11 +39,19 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
         thread = new MainThread(getHolder(), this);
 
         player = new RectPlayer(new Rect(100, 100, 200, 200), Color.rgb(255, 0, 0));
-        playerPoint = new Point(150, 150);
+        playerPoint = new Point(Constants.SCREEN_WIDTH/2, 3* Constants.SCREEN_HEIGHT/4);
+        player.update(playerPoint);
 
-        obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
+        obstacleManager = new ObstacleManager(200, Constants.SCREEN_HEIGHT + 100, 75, Color.BLACK);
 
         setFocusable(true);
+    }
+
+    public void reset() {
+        playerPoint = new Point(Constants.SCREEN_WIDTH/2, 3* Constants.SCREEN_HEIGHT/4);
+        player.update(playerPoint);
+        obstacleManager = new ObstacleManager(200, SCREEN_HEIGHT + 100, 75, Color.BLACK);
+        movingPlayer = false;
     }
 
         @Override
@@ -64,8 +83,20 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 public boolean onTouchEvent (MotionEvent event) {
     switch (event.getAction()) {
         case MotionEvent.ACTION_DOWN:
+            if(!gameOver && player.getRectangle().contains((int)event.getX(), (int)event.getY()))
+                movingPlayer = true;
+            if(gameOver && System.currentTimeMillis() - gameOverTime >= 500) {
+                reset();
+                gameOver = false;
+            }
+            break;
         case MotionEvent.ACTION_MOVE:
-            playerPoint.set((int)event.getX(), (int)event.getY());
+            if(!gameOver && movingPlayer)
+                playerPoint.set((int)event.getX(), (int)event.getY());
+            break;
+        case MotionEvent.ACTION_UP:
+            movingPlayer = false;
+            break;
 
     }
 
@@ -74,16 +105,40 @@ public boolean onTouchEvent (MotionEvent event) {
 }
 
     public void update() {
-
+    if(!gameOver) {
         player.update(playerPoint);
         obstacleManager.update();
+        if(obstacleManager.playerCollide(player)) {
+            gameOver = true;
+            gameOverTime = System.currentTimeMillis();
+        }
+        }
     }
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
+
         canvas.drawColor(Color.WHITE);
 
         player.draw(canvas);
         obstacleManager.draw(canvas);
+
+        if (gameOver) {
+            Paint paint = new Paint();
+            paint.setTextSize(200);
+            paint.setColor(Color.RED);
+            drawCenterText(canvas, paint, "Game Over");
+        }
+    }
+
+    private void drawCenterText(Canvas canvas, Paint paint, String text) {
+        paint.setTextAlign(Paint.Align.LEFT);
+        canvas.getClipBounds(r);
+        int cHeight = r.height();
+        int cWidth = r.width();
+        paint.getTextBounds(text, 0, text.length(), r);
+        float x = cWidth / 2f - r.width() / 2f - r.left;
+        float y = cHeight / 2f + r.height() / 2f - r.bottom;
+        canvas.drawText(text, x, y, paint);
     }
 }
--- app/src/main/java/com/dividedbyzerostudios/candycrawler/ObstacleManager.java
+++ app/src/main/java/com/dividedbyzerostudios/candycrawler/ObstacleManager.java
@@ -1,6 +1,8 @@
 package com.dividedbyzerostudios.candycrawler;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 
 import java.util.ArrayList;
 
@@ -18,6 +20,9 @@ public class ObstacleManager {
 
 
     private long startTime;
+    private long initTime;
+
+    private int score = 0;
 
     public ObstacleManager(int playerGap, int obstacleGap, int obstacleHeight, int color) {
         this.playerGap = playerGap;
@@ -25,11 +30,19 @@ public class ObstacleManager {
         this.obstacleHeight = obstacleHeight;
         this.color = color;
 
-        startTime = System.currentTimeMillis();
+        startTime = initTime = System.currentTimeMillis();
 
         obstacles = new ArrayList<>();
 
         populateObstacles();
+    }
+
+    public boolean playerCollide(RectPlayer player) {
+        for(Obstacle ob : obstacles) {
+            if(ob.playerCollide(player))
+                return true;
+        }
+        return false;
     }
 
     private void populateObstacles() {
@@ -44,7 +57,7 @@ public class ObstacleManager {
     public void update() {
         int elapsedTime = (int) (System.currentTimeMillis() - startTime);
         startTime = System.currentTimeMillis();
-        float speed = Constants.SCREEN_HEIGHT/10000.0f;
+        float speed = (float)(Math.sqrt(1 + (startTime - initTime)/2000.0))*Constants.SCREEN_HEIGHT/(10000.0f); //change the first divider to change speed, higher slows down
         for(Obstacle ob : obstacles) {
             ob.incrementY(speed * elapsedTime);
         }
@@ -52,11 +65,16 @@ public class ObstacleManager {
             int xStart = (int)(Math.random()*(Constants.SCREEN_WIDTH - playerGap));
             obstacles.add(0, new Obstacle(obstacleHeight, color, xStart, obstacles.get(0).getRectangle().top - obstacleHeight - obstacleGap, playerGap));
             obstacles.remove(obstacles.size() - 1);
+            score += 10;
         }
     }
 
     public void draw(Canvas canvas) {
         for(Obstacle ob : obstacles)
             ob.draw(canvas);
+        Paint paint = new Paint();
+        paint.setTextSize(100);
+        paint.setColor(Color.BLACK);
+        canvas.drawText(" " + score, 50, 50 + paint.descent() - paint.ascent(), paint);
     }
 }
